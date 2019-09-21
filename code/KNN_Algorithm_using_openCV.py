2707ac3997348193af0898e2e6177af2a8479a91
p3a <54738745+code-nuce-ai@users.noreply.github.com>
2019-09-21 09:40:50 +0700

    update

---
+++ code/KNN_Algorithm_using_openCV.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np 
+import matplotlib.pyplot as plt 
+# Không sử dụng với windows#############
+import matplotlib                      #
+import tkinter                         #
+matplotlib.use('tkAgg')                #
+########################################
+
+trainData = np.random.randint(0,100,(25,2)).astype(np.float32) 
+print(trainData)
+
+label = np.random.randint(0,2,(25,1)).astype(np.float32)  # tạo random nhãn có giá trị 0 hoặc 1 
+
+squares   = trainData[label.ravel() == 1] # hình vuông được gán nhãn  = 1 
+triangles = trainData[label.ravel() == 0] # hình tam giác được gán nhãn = 0 
+
+targetPredict = np.random.randint(0,100,(1,2)).astype(np.float32)
+
+# vẽ đồ thị 
+plt.scatter(triangles[:,0],triangles[:,1],100,'r','^') #[:,0] lấy các giá trị phía bên trái , [:,1] lấy các giá trị phía bên phải 
+plt.scatter(squares[:,0],squares[:,1],100,'b','s')
+plt.scatter(targetPredict[:,0],targetPredict[:,1],100,'g','o')
+
+knn = cv2.ml.KNearest_create()
+knn.train(trainData,cv2.ml.ROW_SAMPLE,label) #training data với các giá trị tương ứng 
+# xét trường hợp với k = 3 
+temp, result, nearest, distance = knn.findNearest(targetPredict,3)
+############################
+print(trainData[0].shape)
+print(label.shape)
+print(targetPredict.shape)
+# kết quả sau khi training
+print("target to predict : ",targetPredict) # đối tượng cần gán nhãn
+print('result target to predict  : ',format(result)) # kết quả sau khi đã được gán nhãn
+print("nearest with : ",format(nearest)) # đối tượng gán nhãn gần với các tập dữ liệu nào nhất
+print("distance is :",format(distance)) # với khoảng cách gần nhất là bao nhiêu ?
+plt.show()
+print('commit code tutorial')
--- main.py
+++ main.py
@@ -1,35 +1,69 @@
-import cv2
+from PIL import Image 
 import numpy as np 
-import matplotlib.pyplot as plt 
-# Không sử dụng với windows#############
-import matplotlib                      #
-import tkinter                         #
-matplotlib.use('tkAgg')                #
-########################################
+from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
+import cv2 
+import collections
 
-trainData = np.random.randint(0,100,(25,2)).astype(np.float32) 
-print(trainData)
+digits = load_digits() 
 
-label = np.random.randint(0,2,(25,1)).astype(np.float32)  # tạo random nhãn có giá trị 0 hoặc 1 
+# ARRAY TO IMAGE USING PIL
+def convertArrayToImageUsingPIL(source):
+    imageNumber = Image.fromarray(source)
+    imageNumber.show()
 
-squares   = trainData[label.ravel() == 1] # hình vuông được gán nhãn  = 1 
-triangles = trainData[label.ravel() == 0] # hình tam giác được gán nhãn = 0 
+# ARRAY TO IMAGE USING SCIKIT LEARN  + MATPLOTLIB 
+def convertArrayToImageUsingPLT(source):
+    plt.gray()
+    plt.imshow(source)
+    plt.show()
 
-targetPredict = np.random.randint(0,100,(1,2)).astype(np.float32)
+print("database have :",len(digits.images),"pictures")
+def flatMatrix(source):
+    matrix = np.array(source.ravel())
+    return matrix
 
-# vẽ đồ thị 
-plt.scatter(triangles[:,0],triangles[:,1],100,'r','^') #[:,0] lấy các giá trị phía bên trái , [:,1] lấy các giá trị phía bên phải 
-plt.scatter(squares[:,0],squares[:,1],100,'b','s')
-plt.scatter(targetPredict[:,0],targetPredict[:,1],100,'g','o')
+# khởi tạo bộ dữ liệu để training 
+# training 
+
+imageTraining = np.array([(flatMatrix(x)) for x in digits.images[0:1790]]).astype(np.float32)
+labelTraining = np.array([flatMatrix(x)   for x in digits.target[0:1790]]).astype(np.float32)
+
+# index : nhập vị trí chữ số viết tay mà chúng ta cần dự đoán
+
+index = 1795
+
+# khởi tạo ma trận chữ số viết tay cần dự đoán 
+# testing
+
+predictValue = np.array([flatMatrix(digits.images[index])]).astype(np.float32)
+
+# khởi tạo model
 
 knn = cv2.ml.KNearest_create()
-knn.train(trainData,cv2.ml.ROW_SAMPLE,label) #training data với các giá trị tương ứng 
-# xét trường hợp với k = 3 
-temp, result, nearest, distance = knn.findNearest(targetPredict,3)
-# kết quả sau khi training
-print("target to predict : ",targetPredict) # đối tượng cần gán nhãn
-print('result target to predict  : ',format(result)) # kết quả sau khi đã được gán nhãn
-print("nearest with : ",format(nearest)) # đối tượng gán nhãn gần với các tập dữ liệu nào nhất
-print("distance is :",format(distance)) # với khoảng cách gần nhất là bao nhiêu ?
-plt.show()
-print('commit code tutorial')
+knn.train(imageTraining,cv2.ml.ROW_SAMPLE,labelTraining)
+
+# liệt kê các trường hợp của k 
+
+# in ra màn hình chữ số viết tay mà chúng ta cần dự đoán 
+
+convertArrayToImageUsingPLT(digits.images[index])
+
+k = [3,100,170,300,1000,1500,1700]
+for i in k : 
+
+    temp, result, nearest, distance = knn.findNearest(predictValue,i)
+
+    print("====================================")
+    print("k :",i)
+    print("predict value : ",result)
+    print("real value : ",digits.target[index])
+
+    listNearest = [x for x in nearest[0]]
+    findAccuracy = collections.Counter(listNearest)
+    # tính toán độ chính xác 
+    accuracy =  round((findAccuracy[result[0][0]]/sum(findAccuracy.values()))*100,2)
+    print("accuracy : ",accuracy,"%")
+    print("====================================")
+
+
